b8527f01233d09fdd26b827a3070441ad928bf62
Risetech4drew <kanyinjiandy2@gmail.com>
2023-07-12 16:25:35 +0200

    added input field validation for empty value

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const btn = document.getElementById("convert-btn")
 let inputValue = document.getElementById("input-el")
+let inputFieldEl = document.querySelector(".input-area")
 let lengthEl = document.getElementById("length-el")
 let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
+let warningTextEl = document.getElementById("warning-text-el")
+let conversionTextEl = document.querySelectorAll(".conversion-results")
+
 btn.addEventListener("click", function(){
 
-    // converting meters to feet
+    if (inputValue.value){
 
-   
+        lengthEl.textContent = metersAndFeet(inputValue.value)
+        volumeEl.textContent = litersAndGallons(inputValue.value)
+        massEl.textContent = kilogramsAndPounds(inputValue.value)
 
-    lengthEl.textContent = metersAndFeet(inputValue.value)
-    volumeEl.textContent = litersAndGallons(inputValue.value)
-    massEl.textContent = kilogramsAndPounds(inputValue.value)
+        warningTextEl.textContent = ""
+        inputFieldEl.classList.remove("redborder")
+        removeMultpleRedText()
+
+        
+
+    }
+    else{
+        renderWarning()
+    }
 
     inputValue.value = ""
+    
 })
 
 function metersAndFeet(value) {
@@ -37,4 +51,27 @@ function kilogramsAndPounds(value) {
     let poundsToKg = inputValue.value / 2.20462262
 
     return (`${value} kilograms = ${kgsToPounds.toFixed(3)} pounds | ${value} pounds = ${poundsToKg.toFixed(3)} kilograms`)
+}
+
+function renderWarning(){
+
+        inputFieldEl.classList.add("redborder")    
+        warningTextEl.textContent = "Invalid input!"
+        renderMultpleRedText()
+        lengthEl.textContent = "Enter a value in the input field to get conversions"
+        volumeEl.textContent = "Enter a value in the input field to get conversions"
+        massEl.textContent = "Enter a value in the input field to get conversions"
+    
+}
+
+function renderMultpleRedText(){
+    for (let i = 0; i < conversionTextEl.length; i++){
+        conversionTextEl[i].classList.add("redtext")
+    }
+}
+
+function removeMultpleRedText() {
+    for (let i = 0; i < conversionTextEl.length; i++){
+        conversionTextEl[i].classList.remove("redtext")
+    }
 }
